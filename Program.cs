7428e2a1eed24413ad039fc03ab9e42585270124
Anastasiya <96982916+AnastasiyaPoluboyarinova@users.noreply.github.com>
2023-09-24 12:46:26 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO.Pipes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
