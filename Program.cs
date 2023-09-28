773961c1816255a26d9d437cad1362ec06b9cd3d
Anastasiya <96982916+AnastasiyaPoluboyarinova@users.noreply.github.com>
2023-09-28 20:30:57 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,3 +6,11 @@ using System.IO.Pipes;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+
+global using global::System;
+global using global::System.Collections.Generic;
+global using global::System.IO;
+global using global::System.Linq;
+global using global::System.Net.Http;
+global using global::System.Threading;
+global using global::System.Threading.Tasks;
